b819af75d9b71640ab9e82b166433ae708f09760
Dnurrein <dukenurrein@gmail.com>
2022-07-01 13:56:38 -0700

    Assignment

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *main - starts the code
+ *Return: 0
+ */
+
+
+
+int main(void)
+
+	/*Your code goes here*/
+
+{
+
+	char ch = 'a';
+
+
+
+	while (ch <= 'z')
+
+	{
+
+		putchar(ch);
+
+				ch++;
+				}
+
+	putchar('\n');
+
+	return (0);
+
+}
