dd79079da43117ad5a5473c651d6acc398d4bc17
Dnurrein <dukenurrein@gmail.com>
2022-07-07 08:31:44 -0700

    Update 3

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -17,18 +17,18 @@ int main(void)
 
 
 
-			printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 
 
-				i = 0;
+	i = 0;
 
 
 
-					printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 
 
-						return (0);
+	return (0);
 
 }
